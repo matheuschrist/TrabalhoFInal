0e899569d4f4d57709be32b863f46b440d82a32c
kellyEntwickler <kellybritodepaula@gmail.com>
2021-05-17 09:19:30 -0300

    Configuração da Api feita - Deêm 'npm i' para instalar a pasta node_modules

---
+++ Desenvolvimento/Back-end/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = requere('body-parser');
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header', 
+        'Origin, X-Requested-With, Content-tye, Accept, Authorization'
+    );
+
+    if(req.method === 'OPTIONS')
+    {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+})
+
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 400;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
+})
+
+module.exports = app;

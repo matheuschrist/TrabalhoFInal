bbd8dd3406bb560816cc0f43b524d8f4eeba4b82
kellyEntwickler <kellybritodepaula@gmail.com>
2021-06-11 14:44:14 -0300

    Concertei um erro

--- Desenvolvimento/Back-end/src/models/DocumentoRivisao.model.js
+++ Desenvolvimento/Back-end/src/models/DocumentoRivisao.model.js
@@ -1,9 +1,37 @@
-module.exports = class DocumentoRivisao {
-    constructor(descricaoproblema,status,dataconclusao,defeito)
+module.exports = (sequelize, Sequelize) => {
+
+    const documentoRevisao = sequelize.define('DocumentoRevisao',
     {
-        this.Descricaoproblema = descricaoproblema,
-        this.Status = status,
-        this.DataConclusao = dataconclusao,
-        this.Defeito = defeito
-    }
+        DocumentoRevisaoId: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        Descricaoproblema: {
+            type: Sequelize.TEXT,
+            allowNull : false,
+        },
+        Status: {
+            type: Sequelize.INTEGER,
+            allowNull : false,
+        },
+        DataConclusao: {
+            type: Sequelize.DATE,
+            allowNull : false,
+        },
+        Defeito: {
+            type: Sequelize.BOOLEAN,
+            allowNull : false,
+        }
+
+    },
+    {
+        timestamps: false, 
+        tablename: 'DocumentoRevisao', 
+        freezeTableName: true
+    })
+
+    return documentoRevisao
+
 }

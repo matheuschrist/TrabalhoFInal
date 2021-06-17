ad44997daa1d07cd3f7fead95b65a9fb44624fa7
NeoDW <neo.dw@outlook.com>
2021-06-17 11:06:47 -0300

    Acessorio e AcessorioSala concluídos?

--- Desenvolvimento/Back-end/src/routes/Acessorio.routes.js
+++ Desenvolvimento/Back-end/src/routes/Acessorio.routes.js
@@ -2,42 +2,38 @@ const express = require('express')
 const router = express.Router()
 const acessorioController = require('../controllers/Acessorio.controller');
 
-// Cadastra um novo usuário
-/* Exemplo: POST localhost:3000/api/usuarios
+// Cadastra um novo acessório
+/* Exemplo: POST localhost:3000/api/acessorios
 JSON: 
     {
-        "nome": "Deivyd",
-        "login": "deivydw",
-        "senha": "deivyd123",
-        "identificacao": "12345678911",
-        "tipo": 0,
-        "email": "deivyd.email@gmail.com"
+        "tipo": "Mouse",
+        "quantidade": "30"
     }
 */ 
 router.post('/', acessorioController.cadastrar);
 
-// Recupera todos os usuários
-// Exemplo: GET localhost:3000/api/usuarios/
+// Recupera todos os acessórios
+// Exemplo: GET localhost:3000/api/acessorios/
 router.get('/', acessorioController.listarTodos);
 
-// Recupera um usuário pela Id
-// Exemplo: GET localhost:3000/api/usuarios/1
+// Recupera um acessório pela Id
+// Exemplo: GET localhost:3000/api/acessorios/id/1
 router.get('/id/:id', acessorioController.listarId);
 
-// Pesquisa usuários de acordo com o parâmetro inserido
-// Exemplo: GET localhost:3000/api/usuarios/pesquisa/nome?=Deivyd
+// Pesquisa acessório de acordo com o parâmetro inserido
+// Exemplo: GET localhost:3000/api/acessorios/pesquisa/tipo?=Mouse
 router.get('/pesquisar', acessorioController.pesquisar);
 
-// Atualiza um usuário pela Id
-// Exemplo: PUT localhost:3000/api/usuarios/1
+// Atualiza um acessório pela Id
+// Exemplo: PUT localhost:3000/api/acessorios/1
 router.put('/:id', acessorioController.atualizar);
 
-// Exclui um usuário pela Id (para teste)
-// Exemplo: DELETE localhost:3000/api/usuarios/1
+// Exclui um acessório pela Id (para teste)
+// Exemplo: DELETE localhost:3000/api/acessorios/1
 router.delete('/:id', acessorioController.excluirId);
 
-// Exclui todos os usuários (para teste)
-// Exemplo: DELETE localhost:3000/api/usuarios/
+// Exclui todos os acessórios (para teste)
+// Exemplo: DELETE localhost:3000/api/acessorios/
 router.delete('/', acessorioController.excluirTodos);
 
 module.exports = router
--- Desenvolvimento/Back-end/src/routes/AcessorioSala.routes.js
+++ Desenvolvimento/Back-end/src/routes/AcessorioSala.routes.js
@@ -2,42 +2,39 @@ const express = require('express')
 const router = express.Router()
 const acessorioSalaController = require('../controllers/AcessorioSala.controller');
 
-// Cadastra um novo usuário
-/* Exemplo: POST localhost:3000/api/usuarios
+// Cadastra um novo AcessorioSala
+/* Exemplo: POST localhost:3000/api/acessoriosSala
 JSON: 
     {
-        "nome": "Deivyd",
-        "login": "deivydw",
-        "senha": "deivyd123",
-        "identificacao": "12345678911",
-        "tipo": 0,
-        "email": "deivyd.email@gmail.com"
+        "quantidadeAcessorio": "3",
+        "acessorioId": "2",
+        "salaId": "2"
     }
 */ 
 router.post('/', acessorioSalaController.cadastrar);
 
-// Recupera todos os usuários
-// Exemplo: GET localhost:3000/api/usuarios/
+// Recupera todos os AcessoriosSala
+// Exemplo: GET localhost:3000/api/acessoriosSala/
 router.get('/', acessorioSalaController.listarTodos);
 
-// Recupera um usuário pela Id
-// Exemplo: GET localhost:3000/api/usuarios/1
+// Recupera um AcessorioSala pela Id
+// Exemplo: GET localhost:3000/api/acessoriosSala/id/1
 router.get('/id/:id', acessorioSalaController.listarId);
 
-// Pesquisa usuários de acordo com o parâmetro inserido
-// Exemplo: GET localhost:3000/api/usuarios/pesquisa/nome?=Deivyd
+// Pesquisa AcessoriosSala de acordo com o parâmetro inserido
+// Exemplo: GET localhost:3000/api/acessoriosSala/pesquisa/idSala?=2
 router.get('/pesquisar', acessorioSalaController.pesquisar);
 
-// Atualiza um usuário pela Id
-// Exemplo: PUT localhost:3000/api/usuarios/1
+// Atualiza um AcessorioSala pela Id
+// Exemplo: PUT localhost:3000/api/acessoriosSala/1
 //router.put('/:id', acessorioSalaController.atualizar);
 
-// Exclui um usuário pela Id (para teste)
-// Exemplo: DELETE localhost:3000/api/usuarios/1
+// Exclui um AcessorioSala pela Id (para teste)
+// Exemplo: DELETE localhost:3000/api/acessoriosSala/1
 router.delete('/:id', acessorioSalaController.excluirId);
 
-// Exclui todos os usuários (para teste)
-// Exemplo: DELETE localhost:3000/api/usuarios/
+// Exclui todos os AcessoriosSala (para teste)
+// Exemplo: DELETE localhost:3000/api/acessoriosSala/
 router.delete('/', acessorioSalaController.excluirTodos);
 
 module.exports = router
